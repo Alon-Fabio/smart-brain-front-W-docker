6ed95889e25a214a3eb73f68250870c0b94817b0
Alon-Fabio <alon.the.fabio@gmail.com>
2020-11-28 18:09:37 +0200

    Front for docker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
+import Modal from "./components/Modals/Modal";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Modal />, document.getElementById("modal-root"));
 registerServiceWorker();
