b2c27d137c2cd3c589c771b61ed6e30133bb5c72
Alon-Fabio <alon.the.fabio@gmail.com>
2020-12-18 21:59:56 +0200

    Update Rank Component with Lambda and Badge

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emoji: "",
+    };
+  }
+
+  componentDidMount() {
+    this.rankEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.entries === prevProps.entries &&
+      this.props.name === prevProps.name
+    ) {
+      return null;
+    }
+    this.rankEmoji(this.props.entries);
+  }
+
+  rankEmoji = (entries) => {
+    fetch(
+      `https://x12q8i6zkb.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((data) => data.json())
+      .then((emoji) => {
+        if (emoji.input) {
+          this.setState({ emoji: emoji.input });
+        }
+      })
+      .catch((err) => console.log);
+  };
+  // ({ name, entries })
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f2">{`Your Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default Rank;
